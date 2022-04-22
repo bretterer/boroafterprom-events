efd48060b2dd31a8e40046d0bfc3101f71263ea5
Brian Retterer <bretterer@gmail.com>
2022-04-22 11:46:47 -0400

    Closing ticket sales

--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -1,9 +1,12 @@
 <x-main-layout>
 <div class="bg-gray-50 min-h-full">
         <div class="max-w-2xl mx-auto pt-16 pb-24 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-            <h2 class="sr-only">Buy Tickets</h2>
+            <h2 class="text-2xl font-bold mb-10">Buy Afterprom Tickets Tickets</h2>
 
-            <livewire:payment-form />
+            <div class="space-y-6">
+            <p>Thank you for your purchasing your afterprom tickets! Please make sure to bring your ticket with you for checkin at South Metro.</p>
+            <p>If you have not picked up your tickets yet, don't worry, please bring your order conformation email with you to afterprom checkin.</p>
+            </div>
         </div>
     </div>
 </x-main-layout>
--- resources/views/tickets/success.blade.php
+++ resources/views/tickets/success.blade.php
@@ -11,7 +11,7 @@
                     </div>
                     <div class="mt-3 text-center sm:mt-5">
                         <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Order successful</h3>
-                        <div class="mt-2">
+                        <div class="mt-2 ">
                             <p class="text-lg text-gray-500">THIS IS NOT YOUR TICKET!</p>
                             <p class="text-sm text-gray-500">Pickup will happen at lunch <span class="font-bold text-xl">April 21</span> and <span class="font-bold text-xl">April 22</span> at lunch, Please take a screenshot of this page for your ticket pickup!</p>
                             @if($student->payment_type == "cash")
