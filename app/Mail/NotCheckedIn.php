3ed173fb2a748d746c4a0ce1071e4a046624f0ef
Brian Retterer <bretterer@gmail.com>
2023-04-14 23:01:14 -0400

    Update send notices

---
+++ app/Mail/NotCheckedIn.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Attendee;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NotCheckedIn extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Attendee $attendee;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Attendee $attendee)
+    {
+        $this->attendee = $attendee;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Boro Afterprom - Student Not Checked In',
+            from: new Address(
+                address: 'info@boroafterprom.com',
+                name: 'Springboro Afterprom'
+            )
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.not-checked-in'
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
